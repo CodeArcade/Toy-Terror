236f4204d77c2879213074aa3c13852bd9049f90
Cyklan <37187056+Cyklan@users.noreply.github.com>
2020-08-01 13:48:01 +0200

    Particle Engine

---
+++ Engines/ParticleEngine.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using brackeys_2020_2_jam.Models;
+
+namespace brackeys_2020_2_jam
+{
+    public class ParticleEngine : Component {
+        private Random Random;
+        public Vector2 EmitterLocation { get; set; }
+        private List<Particle> Particles;
+        private List<Texture2D> Textures;
+
+        public ParticleEngine(Vector2 emitterLocation, List<Texture2D> textures)
+        {
+            Random = new Random();
+            Particles = new List<Particle>();
+            EmitterLocation = emitterLocation;
+            Textures = textures;
+        }
+
+        public void GenerateNewParticle(Color color) {
+            Texture2D texture = Textures[Random.Next(Textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                1f * (float)(Random.NextDouble() * 2 - 1),
+                1f * (float)(Random.NextDouble() * 2 - 1)
+            );
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(Random.NextDouble() * 2 - 1);
+            float size = (float) Random.NextDouble();
+            int ttl = 20 + Random.Next(40);
+
+            Particle p = new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+            Particles.Add(p);
+        }
+
+        public void Update() {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            spriteBatch.End();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            for (int i = Particles.Count; i >= 0; i--) {
+                Particles[i].Update(gameTime);
+                if (Particles[i].TTL <= 0) {
+                    Particles.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
--- Models/Particle.cs
+++ Models/Particle.cs
@@ -3,8 +3,43 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace brackeys_2020_2_jam.Models
 {
-    public class Particle
+    public class Particle : Component
     {
+        public Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public float Angle { get; set; }
+        public float AngularVelocity { get; set; }
+        public Color Color { get; set; }
+        public float Size { get; set; }
+        public int TTL { get; set; }
 
+        public Particle(Texture2D texture, Vector2 position, Vector2 velocity, float angle, float angularVelocity, Color color, float size, int ttl)
+        {
+            Texture = texture;
+            Position = position;
+            Velocity = velocity;
+            Angle = angle;
+            AngularVelocity = angularVelocity;
+            Color = color;
+            Size = size;
+            TTL = ttl;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, 0, Texture.Width, Texture.Height);
+            Vector2 origin = new Vector2(Texture.Width / 2, Texture.Height / 2);
+
+            spriteBatch.Draw(Texture, Position, sourceRectangle, Color,
+            Angle, origin, Size, SpriteEffects.None, 0f);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            TTL -= 1 * gameTime.ElapsedGameTime.Seconds;
+            Position += Velocity * gameTime.ElapsedGameTime.Seconds;
+            Angle += AngularVelocity * gameTime.ElapsedGameTime.Seconds;
+        }
     }
 }
