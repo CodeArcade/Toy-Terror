d6bc441d82eb574355e757398b307962e1d87a7f
Noctus Rex <noxjudex@gmail.com>
2020-08-03 20:36:27 +0200

    seperated conveyor hitbox and conveyor animation, changed walking particle count

---
+++ Component/Sprites/Environment/Conveyor.cs
@@ -0,0 +1,18 @@
+﻿using brackeys_2020_2_jam.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace brackeys_2020_2_jam.Component.Sprites.Environment
+{
+    public class Conveyor : Sprite
+    {
+        
+        public Conveyor()
+        {
+            Texture = ContentManager.ButtonTexture;
+           // AnimationManager.Play(new Animation(ContentManager.WalkingAnimation, 29));
+        }
+
+    }
+}
